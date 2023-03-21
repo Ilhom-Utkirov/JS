dc1510a98fe76ec7b1e14a87843cb15cfd90432c
Ilhom <inpiration9595@gmail.com>
2023-03-21 22:53:23 +0100

    lab1 added

--- exercise.js
+++ exercise.js
@@ -1,10 +1,18 @@
 'use strict';
-
+// import not works in nodejs
 const dayjs = require('dayjs'); //not works by default in node.js
 
-const currentDate = dayjs('2023-03-07');
-console.log(currentDate);
 
+// let wow = dayjs('2019-01-25').add(1, 'day').subtract(1, 'year').year(2009);
+// print(wow)
+
+// month not correct $M starts from 0
+// one hour before $H start from UTC time Italy 1 hour before
+const currentDate = dayjs('2023-03-07');
+console.log(`current date = ${currentDate}`);
+
+//why to the end score? 
+// text, author,date (score null)then  date is written to the score
 function Answer(text, name, date, score = 0){
     this.text = text;
     this.name = name;
@@ -36,12 +44,15 @@ function Question(text, name, date){
 
 const question = new Question('is js better?', 'Luigi', '2023-02-97');
 
-const firstanswer = new Answer('Yes', 'Luca', '2023-02-15');
+const firstanswer = new Answer('Yes', 'Luca', '2023-02-15', 4);
 const secondanswer = new Answer('Yes', 'Luca', '2023-02-15');
-const thirdanswer = new Answer('No', 'Luigi', '2023-02-15');
+const fourthanswer = new Answer('Never', 'Luca', '2023-02-15', -10);
+const thirdanswer = new Answer('No in millin year', 'Luigi', '2023-02-15');
 
 question.add(firstanswer);
 question.add(secondanswer);
 question.add(thirdanswer);
+question.add(fourthanswer);
 
 console.log(question);
+console.log(firstanswer);
